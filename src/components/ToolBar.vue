499efc2bcb1596726d17fafc4f4622bfc7c070cf
thanawatumarsa <thanawatumarsa@gmail.com>
2016-12-14 19:50:00 +0700

    test Drop Down 2

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,13 +1,19 @@
 <template>
+  <menu :cate-search = "cate-search"></menu>
   <input type="text" v-model="keyword" placeholder="&#128269" @keyup.enter='search(keyword)'>
 </template>
 
 <script>
+import Menu from '../components/Menu'
+
 export default {
-  props: ['search'],
+  props: ['search', 'cate-search'],
   data () {
     return {
       keyword: ''
+    },
+    components {
+      Menu
     }
   }
 }
